8f3791b3a3e46c8e3dc8724dc3bfe957aa3059af
Kajeevan J <it21286032@my.sliit.lk>
2023-10-23 14:27:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 import streamlit as st
 import joblib
 import pandas as pd
-# other necessary imports ...
-
-# Importing functions from the notebook-converted script
 from irwa_fine_tech import preprocess_text, extract_product_name, generate_response
 
-# Load the trained model and data from the respective folders
+# Load trained model and data
 intent_classifier = joblib.load('model/intent_classifier.pkl')
 product_df = pd.read_csv('dataset/realistic_product_data.csv')
 conversational_df = pd.read_csv('dataset/realistic_conversational_data.csv')
 
-
-# Streamlit app interface
+# Streamlit app interface configuration
 st.set_page_config(
     page_title="FineTech Assistant",
     page_icon="🤖",
@@ -21,7 +17,7 @@ st.set_page_config(
 )
 
 # Design header
-col1, col2, col3 = st.beta_columns([1,6,1])
+col1, col2, col3 = st.columns([1,6,1])
 with col2:
     st.image("image/logo1.png", width=200)
     st.title('FineTech Assistant 🤖')
@@ -33,18 +29,33 @@ st.sidebar.image("image/logo2.png", width=200)
 st.sidebar.write("FineTech is your one-stop destination for all tech products. From the latest smartphones to cutting-edge laptops, we've got it all!")
 st.sidebar.write("Have questions? Ask our chatbot here or visit our [website](#)!")  # Replace # with your website link
 
-# Chatbox interaction
-chat_container = st.beta_container()
+# Initialize chat history in session state
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
 
-user_input = st.text_input("Type your question...")
+# Display chat history
+for message in st.session_state.chat_history:
+    role, content = message["role"], message["content"]
+    with st.chat_message(role):
+        st.write(content)
 
+# Accept user input
+user_input = st.chat_input("Type your question...")
 if user_input:
-    response = generate_response(user_input)
-    with chat_container:
-        st.markdown(f"💬 **You**: {user_input}")
-        st.markdown(f"🤖 **FineTech Assistant**: {response}")
+    # Add user message to chat history
+    st.session_state.chat_history.append({"role": "user", "content": user_input})
 
-# A footer or any additional branding can be added here
+    # Generate bot response
+    response = generate_response(user_input)
+    
+    # Display bot's response
+    with st.chat_message("assistant"):
+        st.write(response)
+    
+    # Add bot response to chat history
+    st.session_state.chat_history.append({"role": "assistant", "content": response})
+
+# Footer branding
 st.write("---")
 st.write("© 2023 FineTech - Bringing technology closer to you!")
 st.write("[Privacy Policy](#) | [Terms of Service](#)")  # Replace # with actual links if available
