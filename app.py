e6d60021bcf3ffd4278934458bbe9c5e8f4afa03
Kajeevan J <it21286032@my.sliit.lk>
2023-10-23 14:06:51 +0530

    updated files

--- app.py
+++ app.py
@@ -1,1 +1,50 @@
+import streamlit as st
+import joblib
+import pandas as pd
+# other necessary imports ...
 
+# Importing functions from the notebook-converted script
+from irwa_fine_tech import preprocess_text, extract_product_name, generate_response
+
+# Load the trained model and data from the respective folders
+intent_classifier = joblib.load('model/intent_classifier.pkl')
+product_df = pd.read_csv('dataset/realistic_product_data.csv')
+conversational_df = pd.read_csv('dataset/realistic_conversational_data.csv')
+
+
+# Streamlit app interface
+st.set_page_config(
+    page_title="FineTech Assistant",
+    page_icon="🤖",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+
+# Design header
+col1, col2, col3 = st.beta_columns([1,6,1])
+with col2:
+    st.image("image/logo1.png", width=200)
+    st.title('FineTech Assistant 🤖')
+    st.write("Welcome to FineTech! Your tech shopping assistant. Ask me anything!")
+
+# Sidebar with company info and branding
+st.sidebar.title("FineTech")
+st.sidebar.image("image/logo2.png", width=200)
+st.sidebar.write("FineTech is your one-stop destination for all tech products. From the latest smartphones to cutting-edge laptops, we've got it all!")
+st.sidebar.write("Have questions? Ask our chatbot here or visit our [website](#)!")  # Replace # with your website link
+
+# Chatbox interaction
+chat_container = st.beta_container()
+
+user_input = st.text_input("Type your question...")
+
+if user_input:
+    response = generate_response(user_input)
+    with chat_container:
+        st.markdown(f"💬 **You**: {user_input}")
+        st.markdown(f"🤖 **FineTech Assistant**: {response}")
+
+# A footer or any additional branding can be added here
+st.write("---")
+st.write("© 2023 FineTech - Bringing technology closer to you!")
+st.write("[Privacy Policy](#) | [Terms of Service](#)")  # Replace # with actual links if available
---
+++ irwa_fine_tech.py
@@ -0,0 +1,69 @@
+
+
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+nltk.download('punkt')
+nltk.download('stopwords')
+
+# Loading the datasets
+product_df = pd.read_csv('realistic_product_data.csv')
+conversational_df = pd.read_csv('realistic_conversational_data.csv')
+
+def preprocess_text(text):
+    tokens = word_tokenize(text.lower())
+    tokens = [t for t in tokens if t not in stopwords.words('english')]
+    stemmer = nltk.PorterStemmer()
+    tokens = [stemmer.stem(t) for t in tokens]
+    return ' '.join(tokens)
+
+conversational_df['ProcessedQuery'] = conversational_df['UserQuery'].apply(preprocess_text)
+
+# Splitting the data
+X_train, X_test, y_train, y_test = train_test_split(conversational_df['ProcessedQuery'], conversational_df['Intent'], test_size=0.2)
+
+# Building the intent classifier
+intent_classifier = make_pipeline(TfidfVectorizer(), StandardScaler(with_mean=False), SVC(kernel='linear'))
+intent_classifier.fit(X_train, y_train)
+
+def extract_product_name(query):
+    for product in product_df['ProductName']:
+        if product.lower() in query.lower():
+            return product
+    return None
+
+def generate_response(user_query):
+    # Predict the intent
+    intent = intent_classifier.predict([preprocess_text(user_query)])[0]
+
+    # Extract product name if any
+    product_name = extract_product_name(user_query)
+
+    if intent == "PriceInquiry" and product_name:
+        price = product_df[product_df['ProductName'] == product_name]['Price_LKR'].values[0]
+        return f"The price of the {product_name} is LKR {price}."
+
+    elif intent == "ProductDetails" and product_name:
+        description = product_df[product_df['ProductName'] == product_name]['Description'].values[0]
+        return f"The {product_name} has the following features: {description}."
+
+    elif intent == "ProductAvailability" and product_name:
+        return f"Yes, we have the {product_name} in stock."
+
+    else:
+        return "I'm sorry, I couldn't understand that. Can you please rephrase or ask something else?"
+
+user_query = "How much is the Samsung Galaxy S31?"
+print(generate_response(user_query))
+
+import joblib
+
+# Save the model to disk
+joblib.dump(intent_classifier, 'intent_classifier.pkl')
